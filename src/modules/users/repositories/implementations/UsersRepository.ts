7e54202bbc90f4c2c08d6d3a6d90205a7f59d765
Luan Rodrigues Lima <luanrlima9@gmail.com>
2022-07-04 12:53:17 -0300

    teste model e repositório

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,35 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, { name, email });
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    this.users = this.users.map((user) => {
+      if (user.id === receivedUser.id) {
+        // eslint-disable-next-line no-param-reassign
+        user.admin = true;
+        // eslint-disable-next-line no-param-reassign
+        user.updated_at = new Date();
+      }
+      return user;
+    });
+    return this.users.find((user) => user.id === receivedUser.id);
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
