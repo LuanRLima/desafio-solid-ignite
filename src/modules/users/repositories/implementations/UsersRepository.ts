dc7f4bdfb149948533b8b215e7bec119ecfc836b
Luan Rodrigues Lima <luanrlima9@gmail.com>
2022-07-05 08:21:16 -0300

    desafio concluido

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -20,30 +20,34 @@ class UsersRepository implements IUsersRepository {
 
   create({ name, email }: ICreateUserDTO): User {
     const user = new User();
-    Object.assign(user, { name, email });
+
+    Object.assign(user, {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
     this.users.push(user);
     return user;
   }
-
   findById(id: string): User | undefined {
-    return this.users.find((user) => user.id === id);
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    return this.users.find((user) => user.email === email);
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    this.users = this.users.map((user) => {
-      if (user.id === receivedUser.id) {
-        // eslint-disable-next-line no-param-reassign
-        user.admin = true;
-        // eslint-disable-next-line no-param-reassign
-        user.updated_at = new Date();
-      }
-      return user;
+    Object.assign(receivedUser, {
+      admin: true,
+      updated_at: new Date(),
     });
-    return this.users.find((user) => user.id === receivedUser.id);
+    return receivedUser;
   }
 
   list(): User[] {
